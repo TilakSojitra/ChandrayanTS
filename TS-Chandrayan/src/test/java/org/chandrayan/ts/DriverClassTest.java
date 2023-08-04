018a1e1d0114a25fbcdafe0885828d638d812afb
TilakSojitra <pateltilak9723@gmail.com>
2023-08-04 10:30:50 +0530

    Failed Test Case(Law 1)

--- TS-Chandrayan/src/test/java/org/chandrayan/ts/DriverClassTest.java
+++ TS-Chandrayan/src/test/java/org/chandrayan/ts/DriverClassTest.java
@@ -6,9 +6,25 @@ import static org.junit.Assert.assertEquals;
 
 public class DriverClassTest {
     DriverClass driverClass = new DriverClass();
+    Chandrayan chandrayan = new Chandrayan();
 
     @Test
-    public void testTest(){
-        assertEquals(driverClass.add(2,3),2);
+    public void testTurnUpwards() {
+        assertEquals('U',chandrayan.turnUpwards());
+    }
+
+    @Test
+    public void testTurnDownwards() {
+        assertEquals('D', chandrayan.turnDownwards());
+    }
+
+    @Test
+    public void testTurnLeft() {
+        assertEquals('L',chandrayan.turnLeft());
+    }
+
+    @Test
+    public void testTurnRight() {
+        assertEquals('R',chandrayan.turnRight());
     }
 }
